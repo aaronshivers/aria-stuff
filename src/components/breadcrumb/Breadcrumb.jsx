5e2dc6e2748736b024cd0d8d798e5626f35f0bcc
aaronshivers <aaron@moonmail.org>
2020-05-06 13:38:53 -0500

    use AppRouter in App

---
+++ src/components/breadcrumb/Breadcrumb.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './breadcrumb.scss'
+
+const Links = [
+  {
+    label: 'Home',
+    href: '/'
+  },
+  {
+    label: 'About Us',
+    href: '/about'
+  },
+  {
+    label: 'Contact Us',
+    href: '/contact'
+  },
+]
+
+function Breadcrumb(props) {
+  return (
+    <nav
+      aria-label="Breadcrumb"
+      className="breadcrumb"
+    >
+      <ol>
+        {
+          Links.map((link, i) => (
+            <li key={i}>
+              <Link to={link.href}>
+                { link.label }
+              </Link>
+            </li>
+          ))
+        }
+      </ol>
+    </nav>
+  )
+}
+
+export default Breadcrumb
